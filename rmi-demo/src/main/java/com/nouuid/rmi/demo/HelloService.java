8fb50575a033ee4d2747567ca5e2133368acd812
nouuid <jerryweimail@gmail.com>
2017-03-12 17:22:54 +0800

    rmi

---
+++ rmi-demo/src/main/java/com/nouuid/rmi/demo/HelloService.java
@@ -0,0 +1,16 @@
+package com.nouuid.rmi.demo;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * @author nouuid
+ * @description
+ * @date 3/11/17
+ */
+public interface HelloService extends Remote {
+
+    String say(String name) throws RemoteException;
+
+    int say(int num1, int num2) throws RemoteException;
+}
