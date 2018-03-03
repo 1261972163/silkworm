69ddb6e5fde0ea54fa9cd47522bd876cdad14c31
Jacky Yang <1261972163@qq.com>
2018-03-03 22:14:14 +0800

    Add some annotations in mnist3

--- pengine/src/tensorflow/02_mnist/mnist3/mnist_eval.py
+++ pengine/src/tensorflow/02_mnist/mnist3/mnist_eval.py
@@ -3,10 +3,13 @@
 import time
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
+
+# 加载mnist_inference.py 和mnist_train.py中定义的常量和函数。
 import mnist_inference
 import mnist_train
 
-# 1. 每10秒加载一次最新的模型
+# 每10秒加载一次最新的模型，并且在测试数据上测试最新模型的正确率
 # 加载的时间间隔。
 EVAL_INTERVAL_SECS = 10
 
@@ -16,20 +19,32 @@ def evaluate(mnist):
         y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
         validate_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
 
+        # 直接通过调用封装好的函数来计算前向传播的结果。因为验证测试时不关注正则化损失的值
+        # 所以这里用于计算正则化损失的函数被设置为None。
         y = mnist_inference.inference(x, None)
-        # 分类，预测结果
+        
+        # 使用前向传播的结果计算正确率。如果需要对未知的样例进行分类，那么使用
+        # tf.argmax(y,1)就可以得到输入样例的预测类别了。
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+        # 通过变量重命名的方式来加载模型，这样在前向传播的训练过程中就不需要调用求滑动平均
+        # 的函数来获取平均值了。这样训练过程与验证测试二个过程就可以完全共用mnist_inference.py中定义的
+        # 前向传播过程。
         variable_averages = tf.train.ExponentialMovingAverage(mnist_train.MOVING_AVERAGE_DECAY)
         variables_to_restore = variable_averages.variables_to_restore()
         saver = tf.train.Saver(variables_to_restore)
 
+        # 每隔EVAL_INTERVAL_SECS秒调用一次计算正确率的过程以检验训练过程中正确率的
+        # 变化。这里是个死循环，没考虑主动退出。后续可以考虑通过判断训练过程结束了
+        # 则验证测试过程也可以结束了。
         while True:
             with tf.Session() as sess:
-                # 加载模型
+                # tf.train.get_checkpoint_state函数会通过checkpoint文件自动
+                # 找到目录中最新模型的文件名。
                 ckpt = tf.train.get_checkpoint_state(mnist_train.MODEL_SAVE_PATH)
                 if ckpt and ckpt.model_checkpoint_path:
+                    # 加载恢复模型。
                     saver.restore(sess, ckpt.model_checkpoint_path)
                     # 通过文件名获得迭代轮数
                     global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
--- pengine/src/tensorflow/02_mnist/mnist3/mnist_train.py
+++ pengine/src/tensorflow/02_mnist/mnist3/mnist_train.py
@@ -1,17 +1,22 @@
 # -*- coding:utf-8 -*-
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-import mnist_inference
 import os
 
-# 神经网络参数
+# 加载mnist_inference.py中定义的常量和前向传播的函数。
+import mnist_inference
+
+
+# 配置神经网络训练相关的参数。
 BATCH_SIZE = 100
 LEARNING_RATE_BASE = 0.8
 LEARNING_RATE_DECAY = 0.99
 REGULARIZATION_RATE = 0.0001
-TRAINING_STEPS = 30000
+TRAINING_STEPS = 25000    #怎么执行到最后1轮训练就会出现异常？？
 MOVING_AVERAGE_DECAY = 0.99
-# 持久化路径和
+
+# 模型保存的路径和文件名，通过此数据文件将训练过程与验证测试解耦，二个任务独立运行
+#神经网络训练后的模型参数保存进文件，在验证测试时加载模型参数进行测试
 MODEL_SAVE_PATH="model_data/"
 MODEL_NAME="model"
 
@@ -22,10 +27,12 @@ def train(mnist):
     y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
     # 正则函数
     regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
-    # 向前传播
+    # 直接使用mnist_inference.py中定义的前向传播过程
     y = mnist_inference.inference(x, regularizer)
-    # 训练轮数
+    # 训练轮数，trainable=False指示此参数不参与训练调整
     global_step = tf.Variable(0, trainable=False)
+    
+    # 定义损失函数、学习率、滑动平均操作以及训练过程
     # 使用滑动平均
     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
     variables_averages_op = variable_averages.apply(tf.trainable_variables())
@@ -44,21 +51,28 @@ def train(mnist):
 
     # 将train_step, variables_averages_op合并为一个操作train_op
     # train_op = tf.group([train_step, variables_averages_op])
-    # or
     with tf.control_dependencies([train_step, variables_averages_op]):
         train_op = tf.no_op(name='train')
 
-    # 持久化
+   # 初始化TensorFlow持久化类
     saver = tf.train.Saver()
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
 
+        # 在训练过程中不再测试模型在验证数据上的表现，验证和测试的过程将会有一个独
+        # 立的程序来完成。
         for i in range(TRAINING_STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
             _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_: ys})
             # 每1000轮保存一次模型
             if i % 1000 == 0:
+                # 输出当前的训练情况。这里只输出了模型在当前训练batch上的损失
+                # 函数大小。通过损失函数的大小可以大概了解训练的情况。在验证数
+                # 据集上正确率的信息会有一个单独的程序来生成
                 print("After %d training step(s), loss on training batch is %g." % (step, loss_value))
+                # 保存当前的模型。注意这里给出了global_step参数，这样可以让每个
+                # 被保存的模型的文件名末尾加上训练的轮数，比如“model.ckpt-1000”，
+                # 表示训练1000轮之后得到的模型。这个参数在验证测试时被使用。
                 saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
 
 
